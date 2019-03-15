ba49dac1768d46452428141baef25361891a7a30
Steven Cregan <StevenCregan@gmail.com>
2019-03-15 13:15:12 -0400

    Changed application manager to implement Runnable instead of static application

--- src/main/java/applicationManager.java
+++ src/main/java/applicationManager.java
@@ -9,131 +9,62 @@ import java.util.Properties;
  * This should recognize if "child" processes are running already
  * Management of child processes should be possible through IPC
  */
-public class applicationManager {
+public class applicationManager implements Runnable {
 
     private static volatile boolean running = true;
     private static volatile boolean changedPID = true;
     private static Properties prop = new Properties();
-    private static String appName = "applicationManager";
-    private static String tempFilePath = "com.stevencregan" + File.separator;
-    private static String tempFileName = "appManager.properties";
-    private static String tempFileLocation = (File.separator + tempFilePath + tempFileName);
-    private static String[] programArgs;
+    private String appName;
+    private String packageName;
+    private String tempFilePath;
+    private String tempPropFile;
+    private String tempAppBufferFile;
+    private String[] programArgs;
+    private String parentClassName;
 
-
-    public static void main(String[] args) {
-        PipedInputStream inputPipe = new PipedInputStream();
-        PipedOutputStream outputPipe = new PipedOutputStream();
-        programArgs = args;
-        //Create the PID file, will check if already running first
-        //Will cause exit if already running
-        createPIDFile();
-
-        try {
-            File appMgrBuffer = new File("/tmp" + File.separator + tempFilePath + "appBuffer");
-            //remove any old command buffer file
-            appMgrBuffer.delete();
-            //appMgrBuffer.createNewFile();
-            RandomAccessFile commandBuffer = new RandomAccessFile(appMgrBuffer, "rw");
-            while (running) {
-                //Wait between checks of file
-                Thread.sleep(2500);
-                if (commandBuffer.length() >= 0) {
-                    StringBuilder cmd = new StringBuilder();
-                    while (commandBuffer.length() > 0 && (commandBuffer.getFilePointer() != commandBuffer.length())) {
-                        cmd.append((char) commandBuffer.read());
-                    }
-                    if (cmd.length() > 0) {
-                        System.out.println(cmd);
-                    }
-                    if ((cmd.toString()).contains("stop")) {
-                        System.out.println("Received Stop");
-                        running = false;
-                    }
-                    /*
-                     * if there's content in the command buffer, read it and act accordingly
-                     */
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        System.out.println("Running shutdown hook");
-                        try {
-                            File propFile = new File("/tmp" + tempFileLocation);
-
-                            if (propFile.delete()) {
-                                System.out.println("File (/tmp" + tempFileLocation + ") deleted");
-                            } else
-                                System.out.println("File (/tmp" + tempFileLocation + ") doesn't exist");
-
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                    }
-                })
-        );
-
+    /**
+     * Set Defaults for applicationManager instance
+     */
+    applicationManager() {
+        this.appName = "applicationManager";
+        this.packageName = "";
+        setTempFilePath(this.appName);
+        setTempPropFile(this.tempFilePath);
+        setTempAppBufferFile(this.tempFilePath);
     }
 
     /**
-     * createPIDFile()
-     * Method takes no arguments, and is used to create a tmp file
-     * File should be used to track PIDs of launched children
-     * PIDs will later be used to access pipelines and ensure no duplicate instances
+     * Set Defaults when only given an application name
+     *
+     * @param appName A name for this application, used in creating the tempFolder and files
      */
-    private static void createPIDFile() {
-        InputStream input;
-        OutputStream output;
-        File temp;
-
-        try {
-            String pid = ManagementFactory.getRuntimeMXBean().getName().split("@")[0];
-            System.out.println("Current Process ID is: " + pid);
-
-            if (System.getProperty("os.name").toLowerCase().contains("Windows")) {
-                System.out.println("Running on Windows");
-            } else if (System.getProperty("os.name").toLowerCase().contains("linux")) {
-                System.out.println("Running on linux");
-            } else if (System.getProperty("os.name").toLowerCase().contains("mac")) {
-                System.out.println("Running on Mac");
-                if (new File("/tmp" + tempFileLocation).exists()) {
-                    System.out.println("Temp File Exists");
-                    input = new FileInputStream("/tmp" + tempFileLocation);
-                    // load a properties file
-                    prop.load(input);
-                    // You can only get the property value ONCE!
-                    // So store it to a String!
-                    String managerPID = prop.getProperty("managerPID");
-                    System.out.println("existing Manager PID: " + managerPID);
-                    if (checkAlive(managerPID, appName)) {
-                        queueCommand();
-                    } else {
-                        File propFile = new File("/tmp" + tempFileLocation);
-                        propFile.delete();
-                        System.out.println("File (/tmp" + tempFileLocation + ") deleted");
-                        createPIDFile();
-                    }
-                } else {
-                    temp = new File("/tmp" + tempFileLocation);
-                    temp.getParentFile().mkdirs();
-                    output = new FileOutputStream("/tmp" + tempFileLocation);
-                    // set the properties value
-                    prop.setProperty("managerPID", pid);
-                    // save properties to project root folder
-                    prop.store(output, null);
-                    output.close();
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            running = false;
+    applicationManager(String appName) {
+        if (appName.isEmpty()) {
+            this.appName = "applicationManager";
+        } else {
+            this.appName = appName;
         }
+        setTempFilePath(this.appName);
+        setTempPropFile(this.tempFilePath);
+        setTempAppBufferFile(this.tempFilePath);
+    }
 
+    /**
+     * Set up instance when given an application name and package name
+     *
+     * @param appName     A name for this application, used in creating the tempFolder and files
+     * @param packageName A name for this application's package, used in creating the tempFolder and files
+     */
+    applicationManager(String appName, String packageName) {
+        if (appName.isEmpty()) {
+            this.appName = "applicationManager";
+        } else {
+            this.appName = appName;
+        }
+        this.packageName = packageName;
+        setTempFilePath(this.appName, this.packageName);
+        setTempPropFile(this.tempFilePath);
+        setTempAppBufferFile(this.tempFilePath);
     }
 
     /**
@@ -180,6 +111,119 @@ public class applicationManager {
         return result;
     }
 
+    /* TODO: Change from an executable application to an Interface */
+    public void run() {
+        PipedInputStream inputPipe = new PipedInputStream();
+        PipedOutputStream outputPipe = new PipedOutputStream();
+
+        /* TODO: Separate the callable logic for the PID file creation and running status */
+        //Create the PID file, will check if already running first
+        //Will cause exit if already running
+        createPIDFile();
+
+        /* TODO: Implement an OS compatible version of temp file creation */
+        try {
+            File appMgrBuffer = new File(tempAppBufferFile);
+            //remove any old command buffer file
+            appMgrBuffer.delete();
+            //appMgrBuffer.createNewFile();
+            RandomAccessFile commandBuffer = new RandomAccessFile(appMgrBuffer, "rw");
+            while (running) {
+                //Wait between checks of file
+                Thread.sleep(2500);
+                if (commandBuffer.length() >= 0) {
+                    StringBuilder cmd = new StringBuilder();
+                    while (commandBuffer.length() > 0 && (commandBuffer.getFilePointer() != commandBuffer.length())) {
+                        cmd.append((char) commandBuffer.read());
+                    }
+                    if (cmd.length() > 0) {
+                        System.out.println(cmd);
+                    }
+                    if ((cmd.toString()).contains("stop")) {
+                        System.out.println("Received Stop");
+                        running = false;
+                    }
+                    /*
+                     * if there's content in the command buffer, read it and act accordingly
+                     */
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        System.out.println("Running shutdown hook");
+                        try {
+                            tempFileCleanup(tempPropFile);
+                            tempFileCleanup(tempAppBufferFile);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                })
+        );
+
+    }
+
+    private void tempFileCleanup(String tempFilePath) {
+        File tempFile = new File(tempFilePath);
+        if (tempFile.delete()) {
+            System.out.println("File (" + tempFilePath + ") deleted");
+        } else
+            System.out.println("File (" + tempFilePath + ") doesn't exist");
+    }
+
+    /**
+     * createPIDFile()
+     * Method takes no arguments, and is used to create a tmp file
+     * File should be used to track PIDs of launched children
+     * PIDs will later be used to access pipelines and ensure no duplicate instances
+     */
+    private void createPIDFile() {
+        InputStream input;
+        OutputStream output;
+        File temp;
+
+        try {
+            String pid = ManagementFactory.getRuntimeMXBean().getName().split("@")[0];
+            System.out.println("Current Process ID is: " + pid);
+            if (new File(tempPropFile).exists()) {
+                System.out.println("Temp File Exists");
+                input = new FileInputStream(tempPropFile);
+                // load a properties file
+                prop.load(input);
+                // You can only get the property value ONCE!
+                // So store it to a String!
+                String managerPID = prop.getProperty("managerPID");
+                System.out.println("Potential Existing Manager PID: " + managerPID);
+                if (checkAlive(managerPID, parentClassName)) {
+                    queueCommand();
+                } else {
+                    File propFile = new File(tempPropFile);
+                    propFile.delete();
+                    System.out.println("File (" + tempPropFile + ") deleted");
+                    createPIDFile();
+                }
+            } else {
+                temp = new File(tempPropFile);
+                temp.getParentFile().mkdirs();
+                output = new FileOutputStream(tempPropFile);
+                // set the properties value
+                prop.setProperty("managerPID", pid);
+                // save properties to project root folder
+                prop.store(output, null);
+                output.close();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            running = false;
+        }
+
+    }
 
     /**
      * Queue up a command based on the command line arguments
@@ -187,23 +231,87 @@ public class applicationManager {
      * Since the running instance is reading the buffer file, we just write to the end of it
      * The main instance should read and execute from this file
      */
-    private static void queueCommand() {
-        for (int i = 0; i < programArgs.length; i++) {
-            System.out.println("Arg: " + i + " = " + programArgs[i]);
+    private void queueCommand() {
+        if (programArgs != null) {
+            if (programArgs.length > 0) {
+                for (int i = 0; i < programArgs.length; i++) {
+                    System.out.println("Arg: " + i + " = " + programArgs[i]);
+                }
+                try {
+                    File appMgrBuffer = new File(tempAppBufferFile);
+                    //appMgrBuffer.createNewFile();
+                    RandomAccessFile commandBuffer = new RandomAccessFile(appMgrBuffer, "rw");
+                    commandBuffer.seek(commandBuffer.length());
+                    commandBuffer.writeBytes(programArgs[0] + '\n');
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    System.exit(1);
+                }
+                System.out.println("Placed " + programArgs[0] + " on the Buffer file");
+            }
         }
-        try {
-            File appMgrBuffer = new File("/tmp" + File.separator + tempFilePath + "appBuffer");
-            //appMgrBuffer.createNewFile();
-            RandomAccessFile commandBuffer = new RandomAccessFile(appMgrBuffer, "rw");
-            commandBuffer.seek(commandBuffer.length());
-            commandBuffer.writeBytes(programArgs[0] + '\n');
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.exit(1);
-        }
-        System.out.println("Placed " + programArgs[0] + " on the appMgrBuffer file");
         System.out.println("Closing this instance to avoid duplicates");
         System.exit(0);
+    }
+
+    /**
+     * Create an enumeration of the processes managed by the instance
+     * Should be used to help with autocompletion features,
+     * there should be a -l, --list option to list all children
+     * All children should be updated
+     */
+    public void setManagedProcesses(Enum managedList) {
+        /* TODO: Implement setting and tracking enumerated processes */
+    }
+
+    private void setTempFilePath(String appName) {
+        setTempFilePath(appName, "");
+    }
+
+    private void setTempFilePath(String appName, String packageName) {
+        if (System.getProperty("os.name").toLowerCase().contains("Windows")) {
+            System.out.println("Running on Windows");
+            if (packageName.isEmpty()) {
+                this.tempFilePath = (System.getProperty("java.io.tmpdir") + File.separator + appName);
+            } else {
+                this.tempFilePath = (System.getProperty("java.io.tmpdir") + File.separator + packageName + File.separator + appName);
+            }
+
+        } else if (System.getProperty("os.name").toLowerCase().contains("linux")) {
+            System.out.println("Running on Linux");
+            if (packageName.isEmpty()) {
+                this.tempFilePath = ("/tmp" + File.separator + appName);
+            } else {
+                this.tempFilePath = ("/tmp" + File.separator + packageName + File.separator + appName);
+            }
+        } else if (System.getProperty("os.name").toLowerCase().contains("mac")) {
+            System.out.println("Running on MacOS");
+            if (packageName.isEmpty()) {
+                this.tempFilePath = ("/tmp" + File.separator + appName);
+            } else {
+                this.tempFilePath = ("/tmp" + File.separator + packageName + File.separator + appName);
+            }
+        }
+    }
+
+    private void setTempPropFile(String propFilePath) {
+        this.tempPropFile = propFilePath + File.separator + appName + ".properties";
+    }
+
+    private void setTempAppBufferFile(String appBufferPath) {
+        this.tempAppBufferFile = appBufferPath + File.separator + appName + "Buffer";
+    }
+
+    void setProgramArgs(String[] args) {
+        this.programArgs = args;
+    }
+
+    public String getPID() {
+        return ManagementFactory.getRuntimeMXBean().getName().split("@")[0];
+    }
+
+    void setParentClassName(String parentName) {
+        this.parentClassName = parentName;
     }
 
 }
---
+++ src/main/java/runAppManager.java
@@ -0,0 +1,19 @@
+/**
+ * applicationManager Runner
+ * Created by steven on 2019-03-15.
+ * Use this to start/stop the application manager now
+ */
+public class runAppManager {
+    public static void main(String[] args) {
+        String parentClassName = runAppManager.class.getName();
+        String appName = "";
+        String packageName = "com.stevencregan";
+        applicationManager threadAppMgrTest = new applicationManager(appName, packageName);
+        threadAppMgrTest.setParentClassName(parentClassName);
+        if (args.length > 0) {
+            threadAppMgrTest.setProgramArgs(args);
+        }
+        Thread thread1 = new Thread(threadAppMgrTest, "thread1 - threadAppMgrTest");
+        thread1.start();
+    }
+}
